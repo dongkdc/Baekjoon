eacf25f78fcfdeb30b3c02e9f4423a9e11682d56
dong <dongkdc@naver.com>
2025-12-27 13:26:02 +0900

    2751 수 정렬하기2(M : 새로운 풀이)

--- 2751.c
+++ 2751.c
@@ -1,27 +1,23 @@
 #include<stdio.h>
+#include<stdlib.h>
+
 int main(void){
 
     int N;
     scanf("%d",&N);
 
     int arr[N];
-    for(int i=0;i<N;i++){
+    //int count[1000001]={0};
+    int* count = (int*)calloc(1000001,sizeof(int));
+    
+    for(int i=0;i<N;i++){ 
         scanf("%d",&arr[i]);
+        count[arr[i]] = 1;
     }
-    int temp[N];
-    for(int i=0;i<N;i++){
-        int min = 1000001;
-        int idx=0;
-        for(int j=0;j<N;j++){
-            if(arr[j]<min && arr[j]!=1000001){
-                min = arr[j];
-                idx=j;
-            }
+   
+    for(int i=0;i<=N;i++){
+        if(count[i]==1){
+            printf("%d\n",i);
         }
-        arr[idx]=1000001;
-        temp[i]=min;
-    }
-    for(int i=0;i<N;i++){
-        printf("%d\n",temp[i]);
     }
 }
