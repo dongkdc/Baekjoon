03d310dc2d38503e40c843804c80c064f671ac02
dong <dongkdc@naver.com>
2025-12-28 13:11:18 +0900

    2751 수 정렬하기2 (A: free추가)

--- 2751.c
+++ 2751.c
@@ -3,21 +3,33 @@
 
 int main(void){
 
-    int N;
+    int N,num;
     scanf("%d",&N);
 
-    int arr[N];
-    //int count[1000001]={0};
-    int* count = (int*)calloc(1000001,sizeof(int));
+    int* p_count = (int*)calloc(1000001,sizeof(int));
+    int* m_count = (int*)calloc(1000001,sizeof(int));
     
     for(int i=0;i<N;i++){ 
-        scanf("%d",&arr[i]);
-        count[arr[i]] = 1;
+        scanf("%d",&num);
+        if(num>=0){
+            p_count[num] = 1;
+        }
+        else{
+            m_count[-num] = 1;
+        }
+    }
+
+    for(int i=1000000;i>0;i--){
+        if(m_count[i]==1){
+            printf("-%d\n",i);
+        }
     }
    
-    for(int i=0;i<=N;i++){
-        if(count[i]==1){
+    for(int i=0;i<=1000000;i++){
+        if(p_count[i]==1){
             printf("%d\n",i);
         }
     }
+    free(p_count);
+    free(m_count);
 }
