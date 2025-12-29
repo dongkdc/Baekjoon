dd59a23c01aefb7d2f066a4c4d9cf8ae78bf769c
dong <dongkdc@naver.com>
2025-12-29 16:36:26 +0900

    1181 단어정렬 (M : 형변환 및 동일 문자열 처리방법 수정)

--- 1181.c
+++ 1181.c
@@ -3,8 +3,8 @@
 #include<stdlib.h>
 
 int compare(const void* a, const void* b){
-    int len_a = strlen(a);
-    int len_b = strlen(b);
+    int len_a = strlen((char*)a);
+    int len_b = strlen((char*)b);
 
     if(len_a == len_b){
         return strcmp((char*)a,(char*)b);
@@ -25,14 +25,11 @@ int main(void){
 
     //정렬(동일한 문자열도 포함)
     qsort(arr,N,sizeof(arr[0]),compare);
-
-    for(int i=0;i<N-1;i++){
-        if(strcmp(arr[i],arr[i+1])==0){
-            arr[i][0]= 'A'; // 입력값은 소문자 이므로 대문자를 입력시켜 제외하는 목적
-        }
-    }
-    for(int i=0;i<N;i++){
-        if(arr[i][0]!='A'){
+    
+    printf("%s\n",arr[0]);
+    // 동일한 문자열 제외 출력
+    for(int i=1;i<N;i++){
+        if(strcmp(arr[i],arr[i-1])!=0){
             printf("%s\n",arr[i]);
         }   
     }
