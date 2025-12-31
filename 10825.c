c305b75faceeb20903859fea019350239b824414
dong <dongkdc@naver.com>
2025-12-31 15:59:20 +0900

    10825 국영수 (M : compare 함수 내 가독성을 위한 수정)

--- 10825.c
+++ 10825.c
@@ -13,34 +13,20 @@ int compare(const void* a, const void* b){
     struct Score* SA = (const struct Score*) a;
     struct Score* SB = (const struct Score*) b;
     
-    // 국어 점수 동일한 경우
-    if(SA->Korean == SB->Korean){
-        
-        //국어점수와 영어점수가 동일한 경우 
-        if(SA->English == SB->English){
-
-            //모든 점수가 같은 경우(이름으로 정렬)
-            if(SA->Math == SB->Math){
-                return strcmp(SA->Name,SB->Name);
-            }
-
-            //수학점수로 정렬(내림차순)
-            else{
-                return (SB->Math - SA->Math);
-            }
-        }
-
-        // 영어점수로 정렬(오름차순)
-        else{
-            return (SA->English - SB->English);
-        }
-    }
-
-    // 국어 점수로 정렬(내림차순)
-    else{
-        return (SB->Korean - SA->Korean);
-    }
+   // 1. 국어 점수가 다르면 내림차순
+    if (SA->Korean != SB->Korean) return SB->Korean - SA->Korean;
+    
+    // 2. 영어 점수가 다르면 오름차순
+    if (SA->English != SB->English) return SA->English - SB->English;
+    
+    // 3. 수학 점수가 다르면 내림차순
+    if (SA->Math != SB->Math) return SB->Math - SA->Math;
+    
+    // 4. 위 세 조건이 모두 같으면 이름 오름차순
+    return strcmp(SA->Name, SB->Name);
 }
+
+
 
 int main(void){
     int N;
@@ -54,7 +40,6 @@ int main(void){
         scanf("%s %d %d %d",arr[i].Name,&arr[i].Korean,&arr[i].English,&arr[i].Math);
     }
     
-
     //정렬
     qsort(arr,N,sizeof(arr[0]),compare);
 
