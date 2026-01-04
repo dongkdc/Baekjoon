b3d14b2bbf03b1c250ea7e517a007cb6279cba0b
dong <dongkdc@naver.com>
2026-01-04 14:10:03 +0900

    9095 1,2,3더하기 (M : DP방식으로 구현)

--- 9095.c
+++ 9095.c
@@ -1,32 +1,19 @@
 #include<stdio.h>
 
-int factorial(int n) {
-    if (n <= 1) return 1;
-    int res = 1;
-    for (int i = 2; i <= n; i++) res *= i;
-    return res;
-}
-
 int main(void){
     int T;
     scanf("%d",&T);
+
+    int arr[11]={1,2,4}; // 1,2,3의 출력을 미리 채우기
+    
+    for(int i=3;i<11;i++){
+        arr[i]=arr[i-3]+arr[i-2]+arr[i-1];
+    }
+
     
     for(int i=0;i<T;i++){
-        int num,cnt=0;
+        int num;
         scanf("%d",&num);
-        
-        
-        for (int k = 0; k <= num / 3; k++) {       // 3의 개수
-            for (int j = 0; j <= num / 2; j++) {   // 2의 개수
-                for (int i = 0; i <= num; i++) {   // 1의 개수
-                    if (i * 1 + j * 2 + k * 3 == num) {
-                        int n = i+j+k;
-                        int p = factorial(n) / (factorial(i) * factorial(j) * factorial(k));
-                        cnt += p;
-                    }
-                }
-            }
-        }
-        printf("%d\n",cnt);
+        printf("%d\n",arr[num-1]);
     }
 }
