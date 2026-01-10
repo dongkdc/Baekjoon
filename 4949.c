961c563d93d43d555a061a4eb123b498fd8fa2c2
dong <dongkdc@naver.com>
2026-01-10 13:52:09 +0900

    4949 균형잡힌 세상 (M : 주석추가)

--- 4949.c
+++ 4949.c
@@ -20,34 +20,43 @@ int main(void){
         arr[len]='\0'; 
         
         for(int i=0;i<len;i++){
-
+            // 열린괄호가 들어온 경우는 무조건 push
             if(arr[i]=='(' || arr[i]=='['){
                 stack[idx++] = arr[i];
             }
 
             else if(arr[i]==')'){
+                // 닫힌 괄호가 짝이 안 맞는 경우
+                // (스택에 처음으로 들어온 경우 || 동일한 열린 괄호가 아닌 경우)
                 if(idx==0 || stack[idx-1] != '('){
                     result = 0;
                     break;
                 }
 
+                // 동일한 열린 괄호인 경우
                 else if(stack[idx-1] == '('){
                     idx--;
                 }
             }
 
             else if(arr[i]==']'){
+                // 닫힌 괄호가 짝이 안 맞는 경우
+                // (스택에 처음으로 들어온 경우 || 동일한 열린 괄호가 아닌 경우)
                 if(idx==0 || stack[idx-1] != '['){
                     result = 0;
                     break;
                 }
 
+                // 동일한 열린 괄호인 경우
                 else if(stack[idx-1] == '['){
                     idx--;
                 }
             }
         }
+        // 닫힌 괄호가 짝이 안 맞는 경우 || 스택에 닫힌 괄호만 들어가 있는 경우
         if(result == 0 || idx != 0) printf("no\n");
+        
+        // 짝이 맞는 경우 && 스택에 아무것도 안 들어가 있는 경우
         else if(result && idx==0) printf("yes\n"); 
     }
 }
