dfce8d0ff968aa2b0996542a3dc67d21981830bb
dong <dongkdc@naver.com>
2026-01-19 19:11:27 +0900

    2346 풍선 터뜨리기 (원형 큐) ( M : 인덱스 처리방식 변환)

--- 2346(que).c
+++ 2346(que).c
@@ -1,11 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-
 int main(void){
-    int n,idx=0,paper=0,cnt;
+    int n,idx=0,paper=0;
     scanf("%d",&n);
-    cnt=n; // 현재 풍선의 개수
 
     int* que = (int*)malloc(n*sizeof(int)); // 풍선을 담을 큐
     int* seq = (int*)malloc(n*sizeof(int)); // 터진 풍선 순서
@@ -18,38 +16,32 @@ int main(void){
     paper = que[0];
     seq[0] = 1; 
     que[idx] = 0; // 제거
-    cnt--;
 
     for(int i=1;i<n;i++){
         if(paper>0){
             for(int j=0;j<paper;j++){
                 idx++;
-                if(que[idx%(n)]==0){ // 이미 터진 풍선일 경우
+                if(idx == n) idx =0;
+                if(que[idx]==0){ // 이미 터진 풍선일 경우
                     j--;
                 }
             }
-            idx=idx%n;
             paper = que[idx];
             seq[i] = idx+1;
             que[idx] = 0; // 제거
-            cnt--;
         }
         
         else{
-            paper = (cnt+1) - ((-paper)%cnt);
-            for(int j=0;j<paper;j++){
-                idx++;
-                if(que[idx%(n)]==0){ // 이미 터진 풍선일 경우
+            for(int j=0;j<-paper;j++){
+                idx--;
+                if(idx == -1) idx = n-1;
+                if(que[idx]==0){ // 이미 터진 풍선일 경우
                     j--;
                 }
             }
-            idx=idx%n;
             paper = que[idx];
             seq[i] = idx+1;
             que[idx] = 0; // 제거
-            cnt--;
-
-
         }
     }
 
