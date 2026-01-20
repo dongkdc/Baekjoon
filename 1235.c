46c2bb6f859dd55945f5f5f98c3db458024d8c95
dong <dongkdc@naver.com>
2026-01-20 16:57:00 +0900

    1235 학생번호 ( P : 주석추가)

--- 1235.c
+++ 1235.c
@@ -16,17 +16,17 @@ int main(){
     while(1){
         int status = 1;
         for(int i=0;i<N;i++){
-            for(int j=i+1;j<N;j++){
+            for(int j=i+1;j<N;j++){ // 자신을 제외한 나머지 번호와 비교
                 if(strcmp(ID[i]+idx,ID[j]+idx)==0){
-                    status = 0;
+                    status = 0; // 같은 번호 발생
                     break;
                 }
             }
             if(status == 0) break;
         }
         cnt++;
-        if(status==1) break;
-        idx--;
+        if(status==1) break; // 지금 서로를 구분가능한 최소임을 나타냄
+        idx--; // 다음 자리수까지 비교
     }
     printf("%d",cnt);
 }
