c0f7b95e45523b3b93e6a8a025ae2c4b73cbd188
dong <dongkdc@naver.com>
2026-01-20 16:21:55 +0900

    1269 대칭 차집합 ( M : 이진탐색 이용)

--- 1269.c
+++ 1269.c
@@ -1,26 +1,48 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-int main(void){
-    int a_num,b_num,cnt=0;
+int compare(const void* a, const void* b){
+    return *(int*)a - *(int*)b;
+}
 
-    scanf("%d %d",&a_num,&b_num);
+int search(int* a,int len,int value){
+    int start,end,mid;
+    start = 0;
+    end = len -1;
 
-    int a_arr[a_num],b_arr[b_num];
+    while(start<=end){
+        mid = (start + end) / 2;
 
-    for(int i=0;i<a_num;i++){ // A의 원소 입력
-        scanf("%d",&a_arr[i]);
-    }
-    for(int i=0;i<b_num;i++){ // B의 원소 입력
-        scanf("%d",&b_arr[i]);
-    }
-    
-    for(int i=0;i<a_num;i++){
-        for(int j=0;j<b_num;j++){
-            if(a_arr[i]==b_arr[j]){ 
-                cnt++;
-                break;
-            }
+        if(a[mid]>value){
+            end = mid -1;
+        }
+        else if(a[mid]<value){
+            start = mid +1;
+        }
+        else{ // 같은 경우
+            return 1;
         }
     }
-    printf("%d",a_num+b_num-(2*cnt)); // 총 차집합의 개수 = A의 원소 개수 + B의 원소 개수 -2*교집합
+    return 0; // 못 찾은 경우
+}
+
+int main(void){
+    int n_a,n_b,cnt=0;
+    scanf("%d %d",&n_a,&n_b);
+
+    int arr[n_a];
+    for(int i=0;i<n_a;i++){
+        scanf("%d",&arr[i]);
+    }
+
+    qsort(arr,n_a,sizeof(int),compare);
+
+    for(int i=0;i<n_b;i++){
+        int num;
+        scanf("%d",&num);
+
+        if(search(arr,n_a,num)==1) cnt++; // A안에 있으면 교집합 개수 +1
+    }
+    
+    printf("%d",n_a+n_b-(2*cnt));
 }
