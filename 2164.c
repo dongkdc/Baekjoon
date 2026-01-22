d923d72b030dc961a2911ef6d799573a567231b7
dong <dongkdc@naver.com>
2026-01-22 23:24:25 +0900

    2164 카드2

---
+++ 2164.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void pop(void){
+    
+}
+
+int main(void){
+    int N,cnt;
+    scanf("%d",&N);
+
+    int* que = (int*)malloc(sizeof(int)*N*2);
+
+    for(int i=0;i<N;i++){
+        que[i] = i+1;
+    }
+    int back = N-1;
+    int front = 0;
+
+    cnt = N;
+    while(cnt!=1){
+        int temp;
+
+        // pop (제일 위 카드)
+        front++;
+        // 두 번째 카드 저장
+        temp = que[front];
+        front++;
+        // push
+        que[++back] = temp;
+        cnt--;
+    }
+    
+    printf("%d",que[front]);
+    free(que);
+}
