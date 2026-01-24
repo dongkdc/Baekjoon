523e8b9b82f2e5b7527675acb8bcac2cf630ace6
dong <dongkdc@naver.com>
2026-01-24 22:44:03 +0900

    1068 트리 ( M : 연결리스트 방식에서 배열 인덱스 기반으로 재구현)

--- 1068.c
+++ 1068.c
@@ -1,106 +1,77 @@
 #include<stdio.h>
-#include<stdlib.h>
 
-typedef struct node{
-    int data;
-    struct node* child;
-    struct node* sibling;
+typedef struct{
+    int child[50];
+    int child_cnt;
 }Node;
 
-Node* find(Node* head, int value){
-    if(!head) return NULL;
+// 노드 지우기 
+void eraser(Node* tree, int idx,int value){
+    Node* root = &tree[idx];
+    if(root == NULL) return;
 
-    if(head->data == value){
-        return head;
-    }
-    // 왼쪽 자식
-    Node* res = find(head->child,value);
-    if(res!=NULL) return res;
-    // 오른쪽 형제
-    return find(head->sibling,value);
-}
-
-void add(Node* head,int value,int d){
-    Node* temp = (Node*)malloc(sizeof(Node));
-    temp->data = d;
-    temp->child = NULL;
-    temp->sibling = NULL;
-    
-    // 원하는 노드 찾기
-    Node* res = find(head,value);
-    if (res == NULL) return;
-    
-    // 첫 자식인 경우
-    if(res->child == NULL){
-        res->child = temp;
-    }
-    // 이미 자식이 있는 경우 -> 형제 노드로 들어감
-    else{
-        res = res->child;
-        while(res->sibling!=NULL){
-            res = res->sibling;
+    for(int i=0;i<root->child_cnt;i++){
+        if(root->child[i]==value){
+            root->child[i] = -1; // 찾았다면 지우기
+            return;
         }
-        res->sibling = temp;
     }
-} 
-
-int count(Node* head){
-    int cnt=0;
-    if(head == NULL) return 0;
-
-    Node* n = head->child;
-    if(n == NULL) return 1 + count(head->sibling);
-
-    return count(head->child) + count(head->sibling);
+    
+    for(int i=0;i<root->child_cnt;i++){
+        if(root->child[i]!=-1 ){
+            eraser(tree,root->child[i],value);
+        }
+    }
 }
 
-void eraser(Node* head,int value){
-    if(head == NULL) return;
+// 단말 노드 찾기
+int count(Node* tree,Node root){
+    int cnt = 0;
 
-    if(head->child != NULL && head->child->data == value){
-        head->child = head->child->sibling;
-        return;
+    if(root.child_cnt == 0 || (root.child[0] == -1 && root.child_cnt == 1)) return 1;
+    
+    int sum=0;
+    for(int i=0;i<root.child_cnt;i++){
+        if(root.child[i] != -1){
+            sum+=count(tree,tree[root.child[i]]);
+        }
     }
-    else if(head->sibling != NULL && head->sibling->data == value){
-        head->sibling = head->sibling->sibling;
-        return;
-    }
-    else{
-        eraser(head->child,value);
-        eraser(head->sibling,value);
-    }
+    return sum;
 }
 
 int main(void){
-    int N;
+    int N,root_idx;
     scanf("%d",&N);
+    
+    Node tree[50];
 
-    Node* head = (Node*)malloc(sizeof(Node));
+    // 초기화
+    for(int i=0;i<N;i++){
+        tree[i].child_cnt = 0;
+    }
 
     for(int i=0;i<N;i++){
         int num;
         scanf("%d",&num);
-
-        // 루트 노드
-        if(num == -1){
-            head->data = i;
-            head->sibling = NULL;
-            head->child = NULL;
-        }
+        // 루트 인덱스 찾기
+        if(num == -1) root_idx = i;
+        
         else{
-            add(head,num,i);
-        } 
+            tree[num].child[tree[num].child_cnt] = i;
+            tree[num].child_cnt++;
+        }
     }
 
     int del;
     scanf("%d",&del);
-
-    eraser(head,del);
-
-    if(del == 0){
+    if(del == root_idx){
         printf("0");
         return 0;
     }
+    else{
+         eraser(tree,root_idx,del);
 
-    printf("%d",count(head));
+        printf("%d",count(tree,tree[root_idx]));
+    }
+   
 }
